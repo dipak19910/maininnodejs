476c0a760ddf1b3aa026fb6ac29a775d630ac886
Dipak Gupta <dipak.gupta@daffodilsw.com>
2016-05-29 11:44:36 +0530

    s

---
+++ server.js
@@ -0,0 +1,31 @@
+var express=require('express');
+var bodyParser=require('body-parser');
+var app=express()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// var routes = require("./routes/routes.js")(app);
+
+var server = app.listen(3000, function () {
+	console.log("Listening on "+getIp()+":"+server.address().port);
+});
+
+function getIp(){
+
+	var os=require('os')
+	var ip=os.networkInterfaces();
+	var numberOfIP=Object.keys(ip);
+	if(numberOfIP.length===1){
+		return "127.0.0.1"	
+	}else{
+		var ipInfo=ip["wlan0"];
+		for (var i = 0; i < ipInfo.length; i++) {
+			var ipDetails=ipInfo[i];
+			if (ipDetails["family"]=== "IPv4") {
+				return ipDetails["address"]
+			};
+
+		};
+	}
+
+}
